f6f9febacc3fee003fe28b91325a1cfe2db7b268
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-08-01 00:45:47 +0900

    Feat: orderItem entity build

--- kotlin-note-1/src/main/java/com/kotlin1/spring/domain/entity/OrderItem.kt
+++ kotlin-note-1/src/main/java/com/kotlin1/spring/domain/entity/OrderItem.kt
@@ -1,2 +1,19 @@
 package com.kotlin1.spring.domain.entity
 
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+class OrderItem{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val orderItemId: Long = 0L
+
+    @Column(nullable = false)
+    val count: Long = 0L
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    val orderId: Long = 0L
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    val itemId: Long = 0L
+}
